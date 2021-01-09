4c7a0e5dc024a80a88098b4cee940fdc74fc7930
Cesar Augusto Tapia <cesart050892@users.noreply.github.com>
2021-01-09 00:47:20 -0600

    class 3

---
+++ class/class-3/webpack.config.js
@@ -0,0 +1,26 @@
+const { join } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry:'./src/app/index.js',
+    output:{
+        path: join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer:{
+        port: 3000
+    },
+    module: {
+        rules:[
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
